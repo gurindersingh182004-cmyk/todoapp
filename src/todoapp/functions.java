46b72ed598be94fa87264de9d190080c17fa1a41
Gurinder Singh <gurindersingh182004@gmail.com>
2026-01-02 16:10:39 -0500

    adding tasks done

--- src/todoapp/functions.java
+++ src/todoapp/functions.java
@@ -1,5 +1,14 @@
 package todoapp;
 
+
+
 public class functions {
+    
+    public static void addtask( String description) {
+        System.out.println("name the task you want: ");
+        String name = todoapp.scanner.nextLine();
+        todoapp.tasks.add( new Task(name));
+        
+    }
 
 }
--- src/todoapp/todoapp.java
+++ src/todoapp/todoapp.java
@@ -1,5 +1,14 @@
 package todoapp;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class todoapp {
 
+    public static ArrayList<Task> tasks = new ArrayList<>();
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args){
+        
+    }
 }
